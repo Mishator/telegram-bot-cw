ad20fe6ee68fb2cee6cbe17fda8343869a41e761
Mishator <voyteh89@mail.ru>
2023-10-31 19:16:37 +0300

    Сделана обработка команды /start тг ботом.

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -28,7 +32,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            Long chatId = update.message().chat().id();
+            if (update.message().text().equals("/start")) {
+                SendMessage message = new SendMessage(chatId, "Добро пожаловать, введите задачу. Пример формата: 01.01.2023 20:00 Сделать домашнюю работу");
+                SendResponse response = telegramBot.execute(message);
+            }
+            logger.info("Сообщение из телеги: " + update.message().text());
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
